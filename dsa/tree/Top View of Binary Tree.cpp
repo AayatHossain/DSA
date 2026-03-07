09c826cb5dea74d54422ea6aed9425949f48847e
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-03-07 17:22:10 +0600

    Create Top View of Binary Tree.cpp

---
+++ dsa/tree/Top View of Binary Tree.cpp
@@ -0,0 +1,47 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    map<int,int> m;
+    vector<int> a;
+    
+    void f(Node* root){
+        queue<pair<Node*,int>> q;
+        q.push({root,0});
+        while(!q.empty()){
+            Node* n = q.front().first;
+            int hd = q.front().second;
+            q.pop();
+            if(m.find(hd)==m.end()){
+                m[hd]=n->data;
+            }
+            if(n->left)q.push({n->left, hd-1});
+            if(n->right)q.push({n->right, hd+1});
+        }
+        
+        for(auto x: m){
+            a.push_back(x.second);
+        }
+    
+    }
+    
+    vector<int> topView(Node *root) {
+        // code here
+        f(root);
+        return a;
+        
+    }
+};
