d748b0650e0af8f4b7cda93a088076e84c2e78e0
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-03-17 02:57:27 +0600

    s

---
+++ dsa/array/31. Next Permutation.cpp
@@ -0,0 +1,41 @@
+//on time and o1 space
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& a) {
+        int n = a.size();
+        int i1 = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (a[i] < a[i + 1]) {
+                i1 = i;
+                break;
+            }
+        }
+        int i2 = -1;
+        if (i1 != -1) {
+            int mx = INT_MAX;
+            for (int i = n-1; i >= i1+1; i--) {
+                if (a[i] > a[i1] && a[i] < mx) {
+                    mx = a[i];
+                    i2 = i;
+                }
+            }
+        }
+
+        if (i1 != -1 && i2 != -1) {
+            int temp = a[i1];
+            a[i1] = a[i2];
+            a[i2] = temp;
+        }
+
+        int i = i1 + 1;
+        int j = n - 1;
+        while (i < j) {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i++;
+            j--;
+        }
+    }
+};
--- dsa/array/contest.cpp
+++ dsa/array/contest.cpp
@@ -6,94 +6,28 @@ signed main()
     cin >> t;
     while (t--)
     {
-        int n, k, p, m;
-        cin >> n >> k >> p >> m;
-         int c = 0;
-        vector<int> a(n + 1);
-        for (int i = 1; i <= n; i++)
-        {
-            cin >> a[i];
-        }
-        if (k == n)
-        {
-            c = m / a[p];
-            cout << c << endl;
-            continue;
-        }
-        
+        int a,b,c,m; cin>>a>>b>>c>>m;
+        int l = lcm(a,lcm(b,c));
+        int lcnt = m/l;
 
-        vector<int> b;
-        b.push_back(0);
-        for (int i = 1; i < p; i++)
-        {
-            b.push_back(a[i]);
-        }
-        for (int i = p + 1; i <= n; i++)
-        {
-            b.push_back(a[i]);
-        }
-        b.push_back(a[p]);
-        if (m - a[p] < 0)
-        {
-            // cout<<"ho";
-            cout << 0 << endl;
-            continue;
-            ;
-        }
-        a = b;
-        m-=a[p];
-        c++;
-        
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        queue<pair<int, int>> q;
-        for (int i = 1; i <= k; i++)
-        {
-            // cout<<a[i]<<endl;
-            pq.push({a[i], i});
-        }
-        for (int i = k + 1; i <= n; i++)
-        {
-            q.push({a[i], i});
-        }
-       
-        //   for(auto x: a){
-        //     cout<<x<<" ";
-        //   }
-        for(auto x: a){
-            cout<<x<<" ";
-        }
-        cout<<endl;
-        while (m > 0 && k < n)
-        {
-            pair<int, int> p1 = pq.top();
-            pair<int, int> p2 = q.front();
+        int abl = lcm(a,b);
+        int abcnt = m/abl;
 
-            m -= p1.first;
-            // cout<<pq.top().first<<endl;
-            pq.pop();
-            if (m < 0)
-                break;
-            q.push(p1);
-            if (p2.second != p)
-            {
-                q.pop();
-                pq.push(p2);
-            }
-            else
-            {
-                m -= p2.first;
-                if (m < 0)
-                    break;
-                c++;
-                q.pop();
-                q.push(p2);
-            }
-        }
-        if (k == n)
-        {
-            c = m / a[p];
-        }
-        cout << c << endl;
+        int bcl = lcm(b,c);
+        int bccnt = m/bcl;
+
+        int cal = lcm(c,a);
+        int cacnt = m/cal;
+
+        int alice = 0, bob = 0, carol = 0;
+
+        
+        cout<<l<<endl;
+        cout<<lcnt<<endl;
+        cout<<abl<<endl;
+        cout<<abcnt<<endl;
+
+
     }
     return 0;
 }
