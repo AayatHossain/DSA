d7833a719a33b84cbe0594cb90fabe94da3c84d0
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-03-17 05:03:42 +0600

    Update Repeat and Missing Number Array.cpp

--- dsa/array/Repeat and Missing Number Array.cpp
+++ dsa/array/Repeat and Missing Number Array.cpp
@@ -1,3 +1,42 @@
+//ON AND O1 XOR SOLUTION
+
+vector<int> Solution::repeatedNumber(const vector<int> &a) {
+    int n = a.size();
+    unsigned int x = 0;
+    for(int i = 1; i <= n; i++){
+        x ^= i;
+        x ^= a[i-1];
+    }
+    vector<int> ans(2,0);
+    x &= -x;
+    for(int i = 0; i < n; i++){
+        if(x&a[i]){
+            ans[1] ^= a[i]; 
+        }else{
+            ans[0] ^= a[i];
+        }
+        
+        
+        if(x&(i+1)){
+            ans[1] ^= (i+1); 
+        }else{
+            ans[0] ^= (i+1);
+        }
+    }
+    
+    for(int i = 0; i < n; i++){
+        if(ans[1]==a[i]){
+            swap(ans[0],ans[1]);
+            break;
+        }
+    }
+    
+    return ans;
+}
+
+
+
+
 //ON AND O1 MY SOLUTION. SUM METHOD
 
 vector<int> Solution::repeatedNumber(const vector<int> &a) {
@@ -20,3 +59,6 @@ vector<int> Solution::repeatedNumber(const vector<int> &a) {
     
     return {aa,b};
 }
+
+
+
