2789725361a4c6eb6aa530546f22e8fee0e969e5
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-03-17 16:43:50 +0600

    s

--- dsa/tree/contest.cpp
+++ dsa/tree/contest.cpp
@@ -2,64 +2,28 @@
 using namespace std;
 #define int long long
 
-int n, m;
-vector<vector<int>> a;
-vector<int> mainmap(26,0);
-
-bool ok(int index, int k) {
-    vector<int> temp = mainmap;
-
-    for(int c = 0; c < 26; c++) {
-        temp[c] -= a[index][c];
-    }
-
-    vector<int> mapcurr(26);
-
-    for(int c = 0; c < 26; c++) {
-        mapcurr[c] = (m - k) * temp[c];
-    }
-
-    for(int c = 0; c < 26; c++) {
-        if(mapcurr[c] < a[index][c]) return false;
-    }
-
-    return true;
-}
-
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> n >> m;
-    a.assign(n, vector<int>(26, 0));
+    int t; cin>>t;
+    while(t--){
+        int a,b,c,m; cin>>a>>b>>c>>m;
+        int lab = lcm(a,b);
+        int lbc = lcm(b,c);
+        int lca = lcm(c,a);
+        int labc = lcm(a,lcm(b,c));
 
-    for(int i = 0; i < n; i++){
-        string s;
-        cin >> s;
+        int ab = m/lab;
+        int bc = m/lbc;
+        int ca = m/lca;
+        int abc = m/labc;
 
-        for(char c : s){
-            a[i][c - 'A']++;
-            mainmap[c - 'A']++;
-        }
+        int alice = 6*(m/a) - 3*ab - 3*ca + 2*abc;
+        int bob = 6*(m/b) - 3*ab - 3*bc + 2*abc;
+        int carol = 6*(m/c) - 3*ca - 3*bc + 2*abc;
+
+        cout<<alice<<" "<<bob<<" "<<carol<<endl;
     }
-
-    for(int i = 0; i < n; i++){
-        int s = 0, e = m, ans = -1;
-
-        while(s <= e){
-            int mid = (s + e) / 2;
-
-            if(ok(i, mid)){
-                ans = mid;
-                s = mid + 1;
-            } 
-            else {
-                e = mid - 1;
-            }
-        }
-
-        cout << ans << " ";
-    }
-
-    cout << endl;
+    
 }
