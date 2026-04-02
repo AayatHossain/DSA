4ec8d34295dfe4cdbd853b685c2784039c38060a
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-04-02 12:22:14 +0600

    Update space_optimization_bit.cpp

--- dsa/array/space_optimization_bit.cpp
+++ dsa/array/space_optimization_bit.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 
 void setbit(int a[], int num){
-    int numIndex = (num >> 5);
-    int numbit = (num & 31);
+    int numIndex = (num >> 5); // num / 32, to check which block is num present in
+    int numbit = (num & 31); // num % 32, to check which bit num should be alloted
     a[numIndex] = (a[numIndex] | (1<<numbit)); 
 }
 
@@ -20,7 +20,7 @@ bool checkbit(int a[], int num){
 signed main(){
     int a,b; cin>>a>>b;
     int n = (b-a) + 1;
-    int* arr = new int(n/32);
+    int* arr = new int[(n/32) + 1]();
     for(int i = a; i <=b; i++){
         if(i%2 == 0 || i%5 == 0){
             setbit(arr,i-a);
