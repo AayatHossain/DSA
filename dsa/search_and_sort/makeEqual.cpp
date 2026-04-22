bf29957bcd8ab7734c9cc20bc44b47119670d65f
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-04-22 20:38:30 +0600

    Update makeEqual.cpp

--- dsa/search_and_sort/makeEqual.cpp
+++ dsa/search_and_sort/makeEqual.cpp
@@ -14,4 +14,31 @@ int findMinimumCost(vector<int> &a, int n)
 }
 
 
+//same complexity but cost = left + right approach
+#include <bits/stdc++.h> 
+
+int findMinimumCost(vector<int> &a, int n)
+{
+	int ans = INT_MAX;
+	int right = 0;
+	for(int i = 0; i < n; i++)right+=a[i];
+
+	int left = 0;
+	sort(a.begin(),a.end());
+	for(int i = 0; i < n; i++){
+		right -= a[i];
+
+		int cl = i*a[i] - left;
+		int cr = right - (n-i-1)*a[i];
+		int c = cl+cr;
+
+		left += a[i];
+
+		ans = min(ans, c);
+	}
+	return ans;
+}
+
+
+
 
