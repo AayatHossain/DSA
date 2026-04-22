ba61b5a421cabbe07862de9b427a05cd703cab27
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-04-22 13:36:04 +0600

    Update Merge Without Extra Space.cpp

--- dsa/search_and_sort/Merge Without Extra Space.cpp
+++ dsa/search_and_sort/Merge Without Extra Space.cpp
@@ -1,5 +1,5 @@
 https://www.geeksforgeeks.org/problems/merge-two-sorted-arrays-1587115620/1
-//ONLOGN AND O1, TWO POINTERS.
+//O(NLOGN+MLOGM) AND O1, TWO POINTERS.
 
 class Solution {
   public:
@@ -17,6 +17,39 @@ class Solution {
         }
         sort(a.begin(),a.end());
         sort(b.begin(),b.end());
+        
+        return;
+        
+    }
+};
+
+
+//O(m+n)*O(log(m+n)) and O1. GAP METHOD
+
+class Solution {
+  public:
+    void mergeArrays(vector<int>& a, vector<int>& b) {
+        // code here
+        
+        int n = a.size();
+        int m = b.size();
+        
+        int gap = (n+m+1)/2;
+        while(1){
+            int left = 0, right = gap;
+            while(right < n + m){
+                if(left < n && right < n && a[left] > a[right]){
+                    swap(a[left],a[right]);
+                }else if(left < n && right >= n && a[left] > b[right-n]){
+                    swap(a[left],b[right-n]);
+                }else if(left >= n && right >= n && b[left-n] > b[right-n]){
+                    swap(b[left-n],b[right-n]);
+                }
+                left++;right++;
+            }
+            if(gap==1)break;
+            gap = (gap+1)/2;
+        }
         
         return;
         
