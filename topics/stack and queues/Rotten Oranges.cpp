c8adea659c69250e4a342ae4cf5cf73b1af6af94
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-05-14 02:16:17 +0600

    Create Rotten Oranges.cpp

---
+++ topics/stack and queues/Rotten Oranges.cpp
@@ -0,0 +1,49 @@
+class Solution {
+  public:
+    vector<int> dx = {1,0,-1,0};
+    vector<int> dy = {0,1,0,-1};
+  
+    int orangesRot(vector<vector<int>>& g) {
+        int n = g.size(); int m = g[0].size();
+        vector<vector<int>> a(n,vector<int>(m,INT_MAX));
+        queue<pair<int,int>> q;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(g[i][j]==2){
+                    q.push({i,j});
+                    a[i][j] = 0;
+                }    
+            }
+        }
+        while(!q.empty()){
+            pair<int,int> p = q.front();
+            q.pop();
+            int ux = p.first;
+            int uy = p.second;
+            
+            for(int i = 0; i < 4; i++){
+                int nx = ux + dx[i];
+                int ny = uy + dy[i];
+                
+                bool valid = nx >= 0 && nx < n && ny >= 0 && ny < m && a[nx][ny] > a[ux][uy]+1
+                    && g[nx][ny]==1;
+                if(valid){
+                    a[nx][ny] = a[ux][uy]+1;
+                    g[nx][ny] = 2;
+                    q.push({nx,ny});
+                }
+            }
+        }
+        int mn = 0;
+        int c=0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(a[i][j] != INT_MAX) mn = max(mn,a[i][j]);
+                if(g[i][j] == 1)c++;
+            }
+        }
+        if(c > 0)return -1;
+        return mn;
+        
+    }
+};
