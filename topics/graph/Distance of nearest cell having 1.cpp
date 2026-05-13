cdc4a7976c7bb71d01260ea206eb6036817ce17d
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-05-14 02:02:08 +0600

    Update Distance of nearest cell having 1.cpp

--- topics/graph/Distance of nearest cell having 1.cpp
+++ topics/graph/Distance of nearest cell having 1.cpp
@@ -1,3 +1,5 @@
+//multisource bfs, T = O(n*m), S = O(n*m)
+
 class Solution {
   public:
     vector<int> dx = {1,0,-1,0};
@@ -6,30 +8,29 @@ class Solution {
     vector<vector<int>> nearest(vector<vector<int>>& g) {
         int n = g.size(); int m = g[0].size();
         vector<vector<int>> a(n,vector<int>(m,INT_MAX));
-        queue<pair<pair<int,int>,int>> q;
+        queue<pair<int,int>> q;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(g[i][j]==1){
-                    q.push({{i,j},0});
+                    q.push({i,j});
                     a[i][j] = 0;
                 }    
             }
         }
         while(!q.empty()){
-            pair<pair<int,int>,int> p = q.front();
+            pair<int,int> p = q.front();
             q.pop();
-            int ux = p.first.first;
-            int uy = p.first.second;
-            int ud = p.second;
+            int ux = p.first;
+            int uy = p.second;
             
             for(int i = 0; i < 4; i++){
                 int nx = ux + dx[i];
                 int ny = uy + dy[i];
                 
-                bool valid = nx >= 0 && nx < n && ny >= 0 && ny < m && a[nx][ny] > ud+1;
+                bool valid = nx >= 0 && nx < n && ny >= 0 && ny < m && a[nx][ny] > a[ux][uy]+1;
                 if(valid){
-                    a[nx][ny] = ud+1;
-                    q.push({{nx,ny},ud+1});
+                    a[nx][ny] = a[ux][uy]+1;
+                    q.push({nx,ny});
                 }
             }
         }
